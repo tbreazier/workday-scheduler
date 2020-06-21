f8ded1ea4299e8f831004c48f769fc178c72d3ea
Tom Breazier <tombreazier@macbook-pro.lan>
2020-06-20 19:09:21 -0500

    fixed styling

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,2 +1,12 @@
+//Global Variables
+
+
+//Display Date under title
 var date = moment().format('dddd, MMMM Do');
 $('#currentDay').text(date);
+
+//Style Background based on time
+
+//Save Events to local storage
+
+//Load local storage
